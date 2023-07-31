55210032c6930402ed0bc23565c0d34e766a26c9
Mateus Lopes Albano <50590861+mateus-lopes@users.noreply.github.com>
2023-07-31 15:41:18 +0000

    metodo Strategy poo

--- index.js
+++ index.js
@@ -1,7 +1,62 @@
-// Editor de Figura - JS
-// Autor: Mateus Lopes Albano
+// ex class: Editor de Figura - JS
+// imort class: SaveJpg, SavePdf, SavePng, ClassFigure, ClassMain
 
-import { ClassFigure } from './modules/ClassFigure.js';
+class SaveJpg {
+    constructor() {
+        // pass
+    }
+
+    save(){
+        return 'jpg'
+    }
+}
+
+class SavePdf {
+    constructor() {
+        // pass
+    }
+
+    save(){
+        return 'pdf'
+    }
+}
+
+class SavePng {
+    constructor() {
+        // passs
+    }
+
+    save(){
+        return 'png'
+    }
+}
+
+class ClassFigure {
+    constructor() {
+        // pass
+    }
+    // save figure
+    saveFigure(type) {
+        console.log('save figure em ' + type)
+        switch (type) {
+            case 'png':
+                const png = new SavePng();
+                png.save('png');
+                break;
+            case 'jpg':
+                const jpg = new SaveJpg();
+                jpg.save('jpg');
+                break;
+            case 'pdf':
+                const pdf = new SavePdf();
+                pdf.save('pdf');
+            default:
+                console.log('formato não suportado');
+                // alert('formato não suportado')
+                break;
+        }
+    }
+}
 
 class ClassMain {
     constructor() {
@@ -10,12 +65,11 @@ class ClassMain {
 
     init() {
         let type = 'png';
-            this.saveFigure(type);
+        this.saveFigure(type);
     }
 
     saveFigure(type) {
-        console.log('save figure em ' + type)
-        classFigure = new ClassFigure();
+        const classFigure = new ClassFigure();
         classFigure.saveFigure(type);
     }
 }
